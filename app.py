0f3affd79f81563000b24e3349d765b2315381b2
Arnav Arya <125291336+United-Arya@users.noreply.github.com>
2026-02-21 18:24:17 +0530

    Convert uploaded image to RGB and update display

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ model = YOLO("yolov8n.pt")
 uploaded_file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+    image = Image.open(uploaded_file).convert("RGB")
     img_array = np.array(image)
 
     results = model(img_array)
 
-    annotated_frame = results[0].plot()
+    res_plotted = results[0].plot()
 
-    st.image(annotated_frame, caption="Detected Image")
+    st.image(res_plotted, caption="Detected Image", use_column_width=True)
 #paste command
 # streamlit run app.py
